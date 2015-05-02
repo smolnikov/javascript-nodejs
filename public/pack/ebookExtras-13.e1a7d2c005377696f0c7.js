f8403860dc674052277f92a00b1d1e3b50dc5d77
Ilya Kantor <iliakan@gmail.com>
2015-05-02 17:09:19 +0300

    deploy

---
+++ public/pack/ebookExtras-13.e1a7d2c005377696f0c7.js
@@ -0,0 +1,25 @@
+webpackJsonp_name_([ 13 ], {
+254: function(e, t) {
+"use strict";
+function n() {
+for (var e = document.querySelectorAll('a[href^="/"]'), t = 0; t < e.length; t++) {
+var n = e[t];
+document.getElementById(n.getAttribute("href")) && n.setAttribute("href", "#" + n.getAttribute("href"));
+}
+}
+function i() {
+for (var e = document.querySelectorAll('a[href^="#"]'), t = 0; t < e.length; t++) {
+var n = e[t];
+n.setAttribute("href", n.getAttribute("href").replace(/\//g, "-"));
+}
+for (var i = document.querySelectorAll("[id]"), t = 0; t < i.length; t++) {
+var r = i[t];
+r.id = r.id.replace(/\//g, "-");
+}
+}
+t.init = function() {
+n(), i();
+};
+}
+});
+//# sourceMappingURL=ebookExtras-13.e1a7d2c005377696f0c7.js.map
