52a5ac2348f6060605a0f1aa7fe279105584b5c2
Ilya Kantor <iliakan@gmail.com>
2015-05-31 15:14:15 +0300

    deploy

---
+++ public/pack/nodejsScreencast.bffdf200ff06ca9445e7.js
@@ -0,0 +1,405 @@
+var nodejsScreencast = webpackJsonp_name_([ 9 ], {
+0: function(e, t, n) {
+"use strict";
+function r() {
+i();
+var e = document.querySelector("[data-newsletter-subscribe-form]");
+e.onsubmit = function(t) {
+t.preventDefault(), c.submitSubscribeForm(e);
+};
+var t = document.querySelector("[data-nodejs-screencast-top-subscribe]");
+t.onclick = function(e) {
+var t = new o();
+t.setContent(u(s));
+var n = t.elem.querySelector("form");
+n.setAttribute("data-newsletter-subscribe-form", "nodejs-top"), n.onsubmit = function(e) {
+e.preventDefault(), c.submitSubscribeForm(n, function() {
+t.remove();
+});
+}, e.preventDefault();
+};
+}
+function i() {
+for (var e = document.querySelectorAll("li[data-mnemo]"), t = 0; t < e.length; t++) {
+var n = e[t], r = n.getAttribute("data-mnemo");
+n.insertAdjacentHTML("beforeEnd", '<div class="lessons-list__download"><div class="lessons-list__popup"><ul class="lessons-list__popup-list"><li class="lessons-list__popup-item"><a data-track-outbound href="/nodejs-screencast/nodejs-mp4-low/' + r + '.mp4">Компактный размер</a></li><li class="lessons-list__popup-item"><a data-track-outbound href="/nodejs-screencast/nodejs-mp4/' + r + '.mp4">Высокое качество</a></li></ul></div></div>');
+}
+for (var i = document.querySelectorAll("a[data-video-id]"), t = 0; t < i.length; t++) {
+var o = i[t];
+o.onclick = function(e) {
+e.preventDefault();
+var t = this.getAttribute("data-video-id");
+window.ga("send", "event", "nodejs-screencast", "open", t, {
+hitCallback: l(function() {
+a(t);
+})
+});
+};
+}
+}
+function a(e) {
+for (var t = [ {
+width: 0,
+height: 0
+}, {
+width: 640,
+height: 390
+}, {
+width: 853,
+height: 510
+}, {
+width: 1280,
+height: 750
+} ], n = 0; n < t.length && !(document.documentElement.clientHeight < t[n].height || document.documentElement.clientWidth < t[n].width); n++) ;
+n--;
+var r = t[n].width, i = t[n].height;
+window.ga("send", "event", "nodejs-screencast", "open", e + "-" + r + "x" + i, {
+hitCallback: l(function() {
+if (0 === n) window.location.href = "//www.youtube.com/watch?v=" + e; else {
+var t = new o();
+t.setContent('<iframe width="' + r + '" height="' + i + '" src="//www.youtube.com/embed/' + e + '" frameborder="0" allowfullscreen></iframe>');
+}
+})
+});
+}
+var o = n(10), s = n(65), u = n(49), c = n(60), l = n(50);
+r();
+},
+28: function(e, t, n) {
+"use strict";
+function r(e) {
+function t(e, t) {
+var n = new CustomEvent(e);
+return n.originalEvent = t, n;
+}
+function n(e, n) {
+var r = t("fail", n);
+r.reason = e, i.dispatchEvent(r);
+}
+function r(e, n) {
+var r = t("success", n);
+r.result = e, i.dispatchEvent(r);
+}
+var i = new XMLHttpRequest(), o = e.method || "GET", s = e.body, u = e.url;
+i.open(o, u, e.sync ? !1 : !0), i.method = o;
+var c = a();
+c && !e.skipCsrf && i.setRequestHeader("X-XSRF-TOKEN", c), "[object Object]" == {}.toString.call(s) && (i.setRequestHeader("Content-Type", "application/json;charset=UTF-8"), 
+s = JSON.stringify(s)), e.noDocumentEvents || (i.addEventListener("loadstart", function(e) {
+i.timeStart = Date.now();
+var n = t("xhrstart", e);
+document.dispatchEvent(n);
+}), i.addEventListener("loadend", function(e) {
+var n = t("xhrend", e);
+document.dispatchEvent(n);
+}), i.addEventListener("success", function(e) {
+var n = t("xhrsuccess", e);
+n.result = e.result, document.dispatchEvent(n);
+}), i.addEventListener("fail", function(e) {
+var n = t("xhrfail", e);
+n.reason = e.reason, document.dispatchEvent(n);
+})), e.raw || i.setRequestHeader("Accept", "application/json"), i.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+var l = e.normalStatuses || [ 200 ];
+return i.addEventListener("error", function(e) {
+n("Ошибка связи с сервером.", e);
+}), i.addEventListener("timeout", function(e) {
+n("Превышено максимально допустимое время ожидания ответа от сервера.", e);
+}), i.addEventListener("abort", function(e) {
+n("Запрос был прерван.", e);
+}), i.addEventListener("load", function(t) {
+if (!i.status) return void n("Не получен ответ от сервера.", t);
+if (-1 == l.indexOf(i.status)) return void n("Ошибка на стороне сервера (код " + i.status + "), попытайтесь позднее.", t);
+var a = i.responseText, o = i.getResponseHeader("Content-Type");
+if (o.match(/^application\/json/) || e.json) try {
+a = JSON.parse(a);
+} catch (t) {
+return void n("Некорректный формат ответа от сервера.", t);
+}
+r(a, t);
+}), setTimeout(function() {
+i.send(s);
+}, 0), i;
+}
+var i = n(23), a = n(77);
+document.addEventListener("xhrfail", function(e) {
+new i.Error(e.reason);
+}), e.exports = r;
+},
+49: function(e, t, n) {
+"use strict";
+function r(e) {
+e.bem = i, e.thumb = a;
+}
+var i = n(78)(), a = n(56).thumb;
+e.exports = function(e, t) {
+return t = t ? Object.create(t) : {}, r(t), e(t);
+};
+},
+50: function(e) {
+"use strict";
+e.exports = function(e) {
+function t() {
+t.wasCalled || (t.wasCalled = !0, e());
+}
+return setTimeout(t, 500), t;
+};
+},
+56: function(e, t) {
+"use strict";
+t.thumb = function(e, t, n) {
+if (!e) return e;
+var r = window.devicePixelRatio;
+t *= r, n *= r;
+var i = 160 >= t && 160 >= n ? "t" : 320 >= t && 320 >= n ? "m" : 640 >= t && 640 >= n ? "i" : 1024 >= t && 1024 >= n ? "h" : "";
+return e.slice(0, e.lastIndexOf(".")) + i + e.slice(e.lastIndexOf("."));
+};
+},
+60: function(e, t, n) {
+"use strict";
+function r(e, t) {
+if (e.elements.email.value) {
+var n = a({
+method: "POST",
+url: e.action,
+body: {
+email: e.elements.email.value,
+slug: e.elements.slug.value
+}
+}), r = e.querySelector('[type="submit"]'), s = new i({
+elem: r,
+size: "small",
+elemClass: "button_loading"
+});
+s.start(), r.disabled = !0, n.addEventListener("loadend", function() {
+s.stop(), r.disabled = !1;
+});
+var u = e.getAttribute("data-newsletter-subscribe-form");
+n.addEventListener("success", function(e) {
+200 == this.status ? (window.metrika.reachGoal("NEWSLETTER-SUBSCRIBE", {
+form: u
+}), window.ga("send", "event", "newsletter", "subscribe", u), new o.Success(e.result.message, "slow"), 
+t && t()) : (window.metrika.reachGoal("NEWSLETTER-SUBSCRIBE-FAIL", {
+form: u
+}), window.ga("send", "event", "newsletter", "subscribe-fail", u), new o.Error(e.result.message));
+});
+}
+}
+var i = n(53), a = n(28), o = n(23);
+t.submitSubscribeForm = r;
+},
+65: function(e, t, n) {
+var r = n(108);
+e.exports = function(e) {
+var t = [], n = {}, i = e || {};
+return function(e, i) {
+t.push("");
+var a = [];
+n.b = function(n, r, i) {
+this && this.block, this && this.attributes || {};
+e.call(this, t, a, n, r, i);
+}, n.e = function(e) {
+var t = this && this.block, i = this && this.attributes || {};
+n.b.call({
+block: function() {
+t && t();
+},
+attributes: r.merge([ i ])
+}, e, !0);
+}, n.b.call({
+block: function() {
+n.e.call({
+block: function() {
+t.push("Курс и новые выпуски скринкаста по Node.JS");
+},
+attributes: {
+"class": "title"
+}
+}, "h3"), n.e.call({
+block: function() {
+t.push("Время от времени я провожу онлайн-курс по Node.JS / IO.JS.");
+},
+attributes: {
+"class": "text"
+}
+}, "p"), n.e.call({
+block: function() {
+t.push("Курс &mdash; это практика, решение задач на Node.JS, изучение современной разработки на нём.");
+}
+}, "p"), n.e.call({
+block: function() {
+t.push("Пришлю уведомление с деталями программы, когда будет открыта запись, и вы сможете решить, интересно ли это вам. Также уведомление будет при новых выпусках скринкаста.");
+}
+}, "p"), n.e.call({
+block: function() {
+t.push('<input type="hidden" value="nodejs" name="slug"/>'), i ? t.push('<input type="hidden"' + r.attr("value", i.email, !0, !1) + ' name="email"/>') : n.b.call({
+block: function() {
+n.e.call({
+attributes: {
+type: "email",
+placeholder: "me@mail.com",
+name: "email",
+"data-modal-autofocus": !0,
+required: !0,
+"class": "control"
+}
+}, "input");
+},
+attributes: {
+"class": "text-input"
+}
+}), n.b.call({
+block: function() {
+n.e.call({
+block: function() {
+t.push("Уведомите меня");
+},
+attributes: {
+"class": "text"
+}
+}, "span");
+},
+attributes: {
+type: "submit",
+"class": "button _action"
+}
+}, "button");
+},
+attributes: {
+"data-newsletter-subscribe-form": "nodejs-bottom",
+onsubmit: "return false",
+action: "/newsletter/subscribe",
+method: "POST",
+"class": "form"
+}
+}, "form");
+},
+attributes: {
+"class": "course-form"
+}
+});
+}.call(this, "bem" in i ? i.bem : "undefined" != typeof bem ? bem : void 0, "user" in i ? i.user : "undefined" != typeof user ? user : void 0), 
+t.join("");
+};
+},
+77: function(e) {
+"use strict";
+e.exports = function() {
+var e = document.cookie.match(/XSRF-TOKEN=([\w-]+)/);
+return e ? e[1] : null;
+};
+},
+78: function(e, t, n) {
+"use strict";
+var r = n(108);
+e.exports = function(e) {
+function t(e, t, n, i, a) {
+var o = a || "div";
+switch (o) {
+case "img":
+n.alt && !n.title && (n.title = ""), n.title && !n.alt && (n.alt = n.title), n.alt || (n.alt = "");
+break;
+
+case "input":
+n.type || (n.type = "text");
+break;
+
+case "html":
+e.push("<!DOCTYPE HTML>");
+break;
+
+case "a":
+n.href || (n.href = "#");
+}
+e.push("<" + o + r.attrs(r.merge([ n ]), !0) + ">"), t && t(), -1 == [ "area", "base", "br", "col", "embed", "hr", "img", "input", "keygen", "link", "menuitem", "meta", "param", "source", "track", "wbr" ].indexOf(o) && e.push("</" + o + ">");
+}
+return e = e || {}, e.prefix = e.prefix || "", e.element = e.element || "__", e.modifier = e.modifier || "_", 
+function(n, r, i, a) {
+var o = this.block, s = this.attributes || {};
+if (!s.class && i && !a) throw Error("Block without class: " + i);
+if (s.class) {
+var u = s.class;
+u instanceof Array && (u = u.join(" ")), u = u.split(" ");
+var c;
+try {
+c = u[0].match(RegExp("^(((?!" + e.element + "|" + e.modifier + ").)+)"))[1];
+} catch (l) {
+throw Error("Incorrect bem class: " + u[0]);
+}
+a ? u[0] = r[r.length - 1] + e.element + u[0] : r[r.length] = c;
+var f = (a ? r[r.length - 1] + e.element : "") + c;
+-1 === u.indexOf(f) && (u[u.length] = f);
+for (var p = 0; p < u.length; p++) {
+var h = u[p];
+h.match(RegExp("^(?!" + e.element + ")" + e.modifier)) ? u[p] = f + h : h.match(RegExp("^" + e.element)) && (r[r.length - 2] ? u[p] = r[r.length - 2] + h : u[p] = r[r.length - 1] + h), 
+u[p].match(RegExp("^" + f + "($|(?=" + e.element + "|" + e.modifier + "))")) && (u[p] = e.prefix + u[p]);
+}
+s.class = u.sort().join(" ");
+}
+t(n, o, s, r, i), a || r.pop();
+};
+};
+},
+108: function(e, t, n) {
+"use strict";
+function r(e) {
+return null != e && "" !== e;
+}
+function i(e) {
+return (Array.isArray(e) ? e.map(i) : e && "object" == typeof e ? Object.keys(e).filter(function(t) {
+return e[t];
+}) : [ e ]).filter(r).join(" ");
+}
+t.merge = function a(e, t) {
+if (1 === arguments.length) {
+for (var n = e[0], i = 1; i < e.length; i++) n = a(n, e[i]);
+return n;
+}
+var o = e.class, s = t.class;
+(o || s) && (o = o || [], s = s || [], Array.isArray(o) || (o = [ o ]), Array.isArray(s) || (s = [ s ]), 
+e.class = o.concat(s).filter(r));
+for (var u in t) "class" != u && (e[u] = t[u]);
+return e;
+}, t.joinClasses = i, t.cls = function(e, n) {
+for (var r = [], a = 0; a < e.length; a++) n && n[a] ? r.push(t.escape(i([ e[a] ]))) : r.push(i(e[a]));
+var o = i(r);
+return o.length ? ' class="' + o + '"' : "";
+}, t.style = function(e) {
+return e && "object" == typeof e ? Object.keys(e).map(function(t) {
+return t + ":" + e[t];
+}).join(";") : e;
+}, t.attr = function(e, n, r, i) {
+return "style" === e && (n = t.style(n)), "boolean" == typeof n || null == n ? n ? " " + (i ? e : e + '="' + e + '"') : "" : 0 == e.indexOf("data") && "string" != typeof n ? (-1 !== JSON.stringify(n).indexOf("&"), 
+n && "function" == typeof n.toISOString, " " + e + "='" + JSON.stringify(n).replace(/'/g, "&apos;") + "'") : r ? (n && "function" == typeof n.toISOString, 
+" " + e + '="' + t.escape(n) + '"') : (n && "function" == typeof n.toISOString, 
+" " + e + '="' + n + '"');
+}, t.attrs = function(e, n) {
+var r = [], a = Object.keys(e);
+if (a.length) for (var o = 0; o < a.length; ++o) {
+var s = a[o], u = e[s];
+"class" == s ? (u = i(u)) && r.push(" " + s + '="' + u + '"') : r.push(t.attr(s, u, !1, n));
+}
+return r.join("");
+}, t.escape = function(e) {
+var t = (e + "").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+return t === "" + e ? e : t;
+}, t.rethrow = function o(e, t, r, i) {
+if (!(e instanceof Error)) throw e;
+if (!("undefined" == typeof window && t || i)) throw e.message += " on line " + r, 
+e;
+try {
+i = i || n(116).readFileSync(t, "utf8");
+} catch (a) {
+o(e, null, r);
+}
+var s = 3, u = i.split("\n"), c = Math.max(r - s, 0), l = Math.min(u.length, r + s), s = u.slice(c, l).map(function(e, t) {
+var n = t + c + 1;
+return (n == r ? "  > " : "    ") + n + "| " + e;
+}).join("\n");
+throw e.path = t, e.message = (t || "Jade") + ":" + r + "\n" + s + "\n\n" + e.message, 
+e;
+}, t.DebugItem = function(e, t) {
+this.lineno = e, this.filename = t;
+};
+},
+116: function() {}
+});
+//# sourceMappingURL=nodejsScreencast.bffdf200ff06ca9445e7.js.map
