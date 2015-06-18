41f51710fd3164de1b34d7750b02fe891c578d06
Ilya Kantor <iliakan@gmail.com>
2015-06-19 01:24:42 +0300

    deploy

---
+++ public/pack/coursesMaterials.735d5dc82435320b067d.js
@@ -0,0 +1,93 @@
+var coursesMaterials = webpackJsonp_name_([ 16 ], {
+0: function(t, e, n) {
+"use strict";
+function r() {
+i();
+}
+function i() {
+var t = document.querySelector("[data-should-notify-materials]"), e = t.closest("form");
+t.onchange = function() {
+var t = o({
+method: "PATCH",
+url: e.action,
+body: {
+id: e.elements.id.value,
+shouldNotifyMaterials: e.elements.shouldNotifyMaterials.checked
+}
+});
+t.addEventListener("success", function() {
+new a.Success("Настройка сохранена.");
+});
+};
+}
+var o = n(28), a = n(23);
+r();
+},
+28: function(t, e, n) {
+"use strict";
+function r(t) {
+function e(t, e) {
+var n = new CustomEvent(t);
+return n.originalEvent = e, n;
+}
+function n(t, n) {
+var r = e("fail", n);
+r.reason = t, i.dispatchEvent(r);
+}
+function r(t, n) {
+var r = e("success", n);
+r.result = t, i.dispatchEvent(r);
+}
+var i = new XMLHttpRequest(), a = t.method || "GET", s = t.body, c = t.url;
+i.open(a, c, t.sync ? !1 : !0), i.method = a;
+var u = o();
+u && !t.skipCsrf && i.setRequestHeader("X-XSRF-TOKEN", u), "[object Object]" == {}.toString.call(s) && (i.setRequestHeader("Content-Type", "application/json;charset=UTF-8"), 
+s = JSON.stringify(s)), t.noDocumentEvents || (i.addEventListener("loadstart", function(t) {
+i.timeStart = Date.now();
+var n = e("xhrstart", t);
+document.dispatchEvent(n);
+}), i.addEventListener("loadend", function(t) {
+var n = e("xhrend", t);
+document.dispatchEvent(n);
+}), i.addEventListener("success", function(t) {
+var n = e("xhrsuccess", t);
+n.result = t.result, document.dispatchEvent(n);
+}), i.addEventListener("fail", function(t) {
+var n = e("xhrfail", t);
+n.reason = t.reason, document.dispatchEvent(n);
+})), t.raw || i.setRequestHeader("Accept", "application/json"), i.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+var l = t.normalStatuses || [ 200 ];
+return i.addEventListener("error", function(t) {
+n("Ошибка связи с сервером.", t);
+}), i.addEventListener("timeout", function(t) {
+n("Превышено максимально допустимое время ожидания ответа от сервера.", t);
+}), i.addEventListener("abort", function(t) {
+n("Запрос был прерван.", t);
+}), i.addEventListener("load", function(e) {
+if (!i.status) return void n("Не получен ответ от сервера.", e);
+if (-1 == l.indexOf(i.status)) return void n("Ошибка на стороне сервера (код " + i.status + "), попытайтесь позднее.", e);
+var o = i.responseText, a = i.getResponseHeader("Content-Type");
+if (a.match(/^application\/json/) || t.json) try {
+o = JSON.parse(o);
+} catch (e) {
+return void n("Некорректный формат ответа от сервера.", e);
+}
+r(o, e);
+}), setTimeout(function() {
+i.send(s);
+}, 0), i;
+}
+var i = n(23), o = n(77);
+document.addEventListener("xhrfail", function(t) {
+new i.Error(t.reason);
+}), t.exports = r;
+},
+77: function(t) {
+"use strict";
+t.exports = function() {
+var t = document.cookie.match(/XSRF-TOKEN=([\w-]+)/);
+return t ? t[1] : null;
+};
+}
+});
+//# sourceMappingURL=coursesMaterials.735d5dc82435320b067d.js.map
