cd984cb5b0d873c6e7422d66a84a785c2524acf3
Ilya Kantor <iliakan@gmail.com>
2015-12-07 00:10:03 +0300

    deploy

---
+++ public/pack/coursesFeedbackList.465b690baef0b44a5ea6.js
@@ -0,0 +1,176 @@
+var coursesFeedbackList = webpackJsonp_name_([ 20 ], {
+0: function(e, t, n) {
+"use strict";
+function r(e) {
+function t() {
+var e = n.filter;
+e.teacherId = i.value, e.stars = c.value, n.reset(e);
+var t = document.querySelector("[data-stars-title].feedback-stat__item_active");
+t && t.classList.remove("feedback-stat__item_active"), c.value && document.querySelector('[data-stars-title="' + c.value + '"]').classList.add("feedback-stat__item_active");
+}
+var n = new a(e || window.FEEDBACK_LIST_INIT), r = document.querySelector("[data-feedback-count]");
+n.elem.addEventListener("feedbackChange", function(e) {
+r.hidden = !1, r.children[0].innerHTML = e.detail.loader.total, r.children[1].innerHTML = s(e.detail.loader.total, "отзыв", "отзыва", "отзывов");
+});
+var o = document.querySelector("[data-feedback-form]"), i = o.elements.teacherId, c = o.elements.stars;
+i.onchange = t, c.onchange = t, document.addEventListener("click", function(e) {
+var n = e.target.closest("[data-stars-title]");
+n && (c.value = n.getAttribute("data-stars-title"), t());
+});
+}
+var a = (n(155), n(172)), s = n(173);
+e.exports = r, window.FEEDBACK_LIST_INIT && r();
+},
+155: function(e, t, n) {
+"use strict";
+function r(e) {
+function t(e, t) {
+var n = new CustomEvent(e);
+return n.originalEvent = t, n;
+}
+function n(e, n) {
+var r = t("fail", n);
+r.reason = e, a.dispatchEvent(r);
+}
+function r(e, n) {
+var r = t("success", n);
+r.result = e, a.dispatchEvent(r);
+}
+var a = new XMLHttpRequest(), o = e.method || "GET", i = e.body, c = e.url;
+a.open(o, c, e.sync ? !1 : !0), a.method = o;
+var u = s();
+u && !e.skipCsrf && a.setRequestHeader("X-XSRF-TOKEN", u), "[object Object]" == {}.toString.call(i) && (a.setRequestHeader("Content-Type", "application/json;charset=UTF-8"), 
+i = JSON.stringify(i)), e.noDocumentEvents || (a.addEventListener("loadstart", function(e) {
+a.timeStart = Date.now();
+var n = t("xhrstart", e);
+document.dispatchEvent(n);
+}), a.addEventListener("loadend", function(e) {
+var n = t("xhrend", e);
+document.dispatchEvent(n);
+}), a.addEventListener("success", function(e) {
+var n = t("xhrsuccess", e);
+n.result = e.result, document.dispatchEvent(n);
+}), a.addEventListener("fail", function(e) {
+var n = t("xhrfail", e);
+n.reason = e.reason, document.dispatchEvent(n);
+})), e.raw || a.setRequestHeader("Accept", "application/json"), a.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+var d = e.normalStatuses || [ 200 ];
+return a.addEventListener("error", function(e) {
+n("Ошибка связи с сервером.", e);
+}), a.addEventListener("timeout", function(e) {
+n("Превышено максимально допустимое время ожидания ответа от сервера.", e);
+}), a.addEventListener("abort", function(e) {
+n("Запрос был прерван.", e);
+}), a.addEventListener("load", function(t) {
+if (!a.status) return void n("Не получен ответ от сервера.", t);
+if (-1 == d.indexOf(a.status)) return void n("Ошибка на стороне сервера (код " + a.status + "), попытайтесь позднее.", t);
+var s = a.responseText, o = a.getResponseHeader("Content-Type");
+if (o.match(/^application\/json/) || e.json) try {
+s = JSON.parse(s);
+} catch (t) {
+return void n("Некорректный формат ответа от сервера.", t);
+}
+r(s, t);
+}), setTimeout(function() {
+a.send(i);
+}, 0), a;
+}
+var a = n(147), s = n(156);
+document.addEventListener("xhrfail", function(e) {
+new a.Error(e.reason);
+}), e.exports = r;
+},
+156: function(e, t) {
+"use strict";
+e.exports = function() {
+var e = document.cookie.match(/XSRF-TOKEN=([\w-]+)/);
+return e ? e[1] : null;
+};
+},
+172: function(e, t, n) {
+"use strict";
+function r(e, t) {
+if (!(e instanceof t)) throw new TypeError("Cannot call a class as a function");
+}
+var a = function() {
+function e(e, t) {
+for (var n = 0; n < t.length; n++) {
+var r = t[n];
+r.enumerable = r.enumerable || !1, r.configurable = !0, "value" in r && (r.writable = !0), 
+Object.defineProperty(e, r.key, r);
+}
+}
+return function(t, n, r) {
+return n && e(t.prototype, n), r && e(t, r), t;
+};
+}(), s = n(155), o = function() {
+function e(t) {
+var n = this, a = t.elem, s = t.filter;
+r(this, e), this.elem = a, this.container = a.querySelector("[data-feedback-container]"), 
+this.baseUrl = "/courses/feedback-fetch", this.reset(s), window.addEventListener("scroll", function(e) {
+return n.onScroll(e);
+});
+}
+return a(e, [ {
+key: "reset",
+value: function(e) {
+this.filter = e, this.count = 0, this.total = null, this.hasMore = !0, this.container.innerHTML = "", 
+this.load();
+}
+}, {
+key: "onScroll",
+value: function() {
+this.hasMore && this.container.getBoundingClientRect().bottom <= document.documentElement.clientHeight && !this.isLoading && this.load();
+}
+}, {
+key: "load",
+value: function() {
+var e = this, t = this.baseUrl + "?skip=" + this.count + "&needTotal=" + (null === this.total ? 1 : 0);
+for (var n in this.filter) t += "&" + n + "=" + this.filter[n];
+var r = s({
+method: "GET",
+json: !0,
+url: t
+});
+this.elem.classList.add("course-feedbacks_loading"), this.isLoading = !0, r.addEventListener("loadend", function() {
+e.isLoading = !1, e.elem.classList.remove("course-feedbacks_loading");
+}), r.addEventListener("success", function(t) {
+void 0 !== t.result.total && (e.total = t.result.total), t.result.count ? (e.container.insertAdjacentHTML("beforeEnd", t.result.html), 
+e.count += t.result.count) : e.count || (e.container.innerHTML = '<p style="text-align:center">Отзывов пока нет.</p>'), 
+t.result.hasMore === !1 && (e.hasMore = !1), e.elem.dispatchEvent(new CustomEvent("feedbackChange", {
+bubbles: !0,
+detail: {
+loader: e
+}
+}));
+});
+}
+} ]), e;
+}();
+e.exports = o;
+},
+173: function(e, t) {
+"use strict";
+function n(e) {
+return e % 10 == 1 && e % 100 != 11 ? "one" : e % 10 >= 2 && 4 >= e % 10 && (12 > e % 100 || e % 100 > 14) && e == Math.floor(e) ? "few" : e % 10 === 0 || e % 10 >= 5 && 9 >= e % 10 || e % 100 >= 11 && 14 >= e % 100 && e == Math.floor(e) ? "many" : "other";
+}
+function r(e, t, r, a) {
+var s = n(e);
+switch (s) {
+case "one":
+return t;
+
+case "few":
+return r;
+
+case "many":
+return a;
+
+default:
+throw Error("Unsupported count: " + e);
+}
+}
+e.exports = r;
+}
+});
+//# sourceMappingURL=coursesFeedbackList.465b690baef0b44a5ea6.js.map
